7d0eb3bed22f4a6670f94628193cba842ec7fc0c
OleMausS <ole.meyer99@gmail.com>
2017-03-15 16:43:59 +0100

    Add files via upload

---
+++ code/Standard_Schema.ino
@@ -0,0 +1,17 @@
+#include "Wire.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_LEDBackpack.h"
+
+Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix(); //definiert den Code für die Bicolor Matrix
+
+void setup() {
+  Serial.begin(9600); //Starten des seriellen Monitors
+  Serial.println("Programmstart"); //zeigen das das Programm beginnt
+  
+  matrix.begin(0x70); //Die 7-Bit-Adresse der Matrix ist 0x70, hätte man mehrere Matrizen so wären die 0x71,0x72... 
+
+}
+
+void loop() {
+
+}
